267a9689c2f06507fa47bb947efb5efd5c944996
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-13 23:53:22 -0500

    Adding Routes

---
+++ src/components/accounts/Account.js
@@ -0,0 +1,10 @@
+// User can view contact information
+// User can edit contact information
+// User can view current projects
+// Makr user can delete a project
+// User can click the logo and route to the home page
+// User can view the logged in nav bar
+
+export const Account = () => {
+
+}
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,5 +1,30 @@
-// Set up all routes
+// Set up all routes for login / register / home / professional profile / account / project request form
+
+import { Outlet, Route, Routes } from "react-router-dom"
+import { Account } from "../accounts/Account"
+import { Login } from "../auth/Login"
+import { Register } from "../auth/Register"
+import { LandingPage } from "../LandingPage"
+import { Makr } from "../Makr"
+import { Profile } from "../profiles/Profile"
+import { ProjectForm } from "../projects/ProjectForm"
 
 export const ApplicationViews = () => {
-
+    return (
+        <Routes>
+            <Route path="/" element={
+                <>
+                    <LandingPage/>
+                    <Outlet/>
+                </>
+            }>
+                <Route path="login" element={<Login/>}/>
+                <Route path="register" element={<Register/>}/>
+                <Route path="home" element={<Makr/>}/>
+                <Route path="profile" element={<Profile/>}/>
+                <Route path="account" element={<Account/>}/>
+                <Route path="form" element={<ProjectForm/>}/>
+            </Route>
+        </Routes>
+    )
 }
