4ee6a4ecb44c2c0b090a90a231f948621be835d0
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-14 16:26:26 -0500

    Adding information to Home Page

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -1,1 +1,29 @@
 // Define and export functions for all fetch calls
+
+// ----------------------------- User Data -------------------------------------
+
+export const getUsersData = () => {
+    return fetch(`http://localhost:8088/users`)
+    .then(res => res.json())
+}
+
+// ----------------------------- Expertise Data -------------------------------------
+
+export const getExpertiseData = () => {
+    return fetch(`http://localhost:8088/expertiseTypes`)
+    .then(res => res.json())
+}
+
+// ----------------------------- Pro Data -------------------------------------
+
+export const getProsData = () => {
+    return fetch(`http://localhost:8088/pros?_expand=expertiseType&_expand=user`)
+    .then(res => res.json())
+}
+
+// ----------------------------- User Data -------------------------------------
+
+
+// ----------------------------- User Data -------------------------------------
+
+
