51c5c38c4ecb6aa1f33f92e7aa916b78e567a7f8
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-14 01:09:57 -0500

    Adding App Module

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,6 +1,17 @@
 // If a current user exists, display the logged in nav bar
 // Else display the logged out nav bar
 
+import { LoggedInNav } from "./LoggedInNav"
+import { LoggedOutNav } from "./LoggedOutNav"
+
 export const NavBar = () => {
-    
+
+    const localUser = localStorage.getItem("current_user")
+    const currentUserObject = JSON.parse(localUser)
+
+    if (currentUserObject) {
+        return <LoggedInNav/>
+    } else {
+        return <LoggedOutNav/>
+    }
 }
