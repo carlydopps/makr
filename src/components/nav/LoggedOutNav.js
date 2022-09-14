fab94c6ef3737ff6d8abdb29133304abe54da075
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-14 00:34:50 -0500

    Adding basic components to the logged out nav bar

--- src/components/nav/LoggedOutNav.js
+++ src/components/nav/LoggedOutNav.js
@@ -1,5 +1,15 @@
 // User who is not logged in can click a sign up or login button in the nav bar that will route to the respective page
 
+import { useNavigate } from "react-router-dom"
+
 export const LoggedOutNav = () => {
 
+    const navigate = useNavigate()
+
+    return (
+        <>
+            <button onClick={() => navigate("/login")} className="navbar__button">Login</button>
+            <button onClick={() => navigate("/register")} className="navbar__button">Register</button>
+        </>
+    )
 }
