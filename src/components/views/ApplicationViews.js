406e7a0233635cb55e1f63b3226f81cb05276d1a
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-15 08:19:50 -0500

    Project Request Form

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -4,30 +4,32 @@ import { Outlet, Route, Routes } from "react-router-dom"
 import { Account } from "../accounts/Account"
 import { LandingPage } from "../home/LandingPage"
 import { Makr } from "../home/Makr"
-import { ProjectForm } from "../projects/ProjectForm"
+import { RequestForm } from "../projects/RequestForm"
 import { ProProfile } from "../pros/ProProfile"
 
 export const ApplicationViews = () => {
+
     return (
         <Routes>
-            (localStorage.user("current_user"))
-            ? <Route path="/" element={
-                <>
-                    <Makr/>
-                    <Outlet/>
-                </>
-            }/>
-            : <Route path="/" element={
-                <>
-                    <LandingPage/>
-                    <Outlet/>
-                </>
-            }/>
-            
+            {
+                localStorage.getItem("current_user")
+                ? <Route path="/" element={
+                    <>
+                        <Makr/>
+                        <Outlet/>
+                    </>
+                }/>
+                : <Route path="/" element={
+                    <>
+                        <LandingPage/>
+                        <Outlet/>
+                    </>
+                }/>
+            }
             <Route path="home" element={<Makr/>}/>
             <Route path="profile/:proId" element={<ProProfile/>}/>
             <Route path="account" element={<Account/>}/>
-            <Route path="request-form/:proId" element={<ProjectForm/>}/>
+            <Route path="request-form" element={<RequestForm/>}/>
         </Routes>
     )
 }
