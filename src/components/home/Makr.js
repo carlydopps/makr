6eeed32ea96eeffa19b22041d111c1af6c120498
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-17 18:01:33 -0500

    Fixing bug with rendering list after registration

--- src/components/home/Makr.js
+++ src/components/home/Makr.js
@@ -2,7 +2,6 @@
 // User who is not logged in can view the logged out nav bar
 // User who is logged in can view the logged in nav bar
 
-import { useEffect } from "react"
 import { ProList } from "../pros/ProList"
 
 export const Makr = () => {
