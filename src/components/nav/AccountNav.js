f90693a70588a2599e0ac76ad2d0037369b3a7bc
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-20 15:39:55 -0500

    Account Menu Bar

--- src/components/nav/AccountNav.js
+++ src/components/nav/AccountNav.js
@@ -4,57 +4,58 @@
 
 import { Menu, MenuItem } from "@mui/material"
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { getCurrentUser } from "../ApiManager"
 
-export const AccountNav = () => {
+export const AccountNav = ({user}) => {
 
-    const [anchor, setAnchor] = useState(false)
-    const [user, setUser] = useState({})
-
-    const localUser = localStorage.getItem("current_user")
-    const currentUser = JSON.parse(localUser)
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [open, setOpen] = useState(false)
 
     const navigate = useNavigate()
 
-    useEffect(
-        () => {
-            getCurrentUser(currentUser.id)
-            .then((data) => {
-                const singleUser = data[0]
-                setUser(singleUser)
-            })
-        },
-        []
-    )
-
     const handleClose = () => {
-        setAnchor(false)
+        setAnchorEl(null)
+        setOpen(false)
     }
 
     const handleClick = (event) => {
-        setAnchor(event.target)
+        setAnchorEl(event.currentTarget)
+        setOpen(true)
     }
 
     return <>
-        <button onClick={handleClick}className="navbar__button navbar__account">
+        <button 
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}className="navbar__button navbar__account">
             <img src={user?.profileImage} alt="Account" className="account-image"></img>
         </button>
         <Menu
-            keepMounted
-            anchor={anchor}
-            onClose={handleClose}
-            open={Boolean(anchor)}>
-                <MenuItem onClick={navigate(`/account/${currentUser.id}`)}
-                className="accountnav-menu menu-account">Account</MenuItem>
-                <MenuItem onClick={() => navigate(`/projects`)}
-                    className="accountnav-menu menu-projects">Projects</MenuItem>
-                <MenuItem onClick={() => {
-                    localStorage.removeItem("current_user")
-                    navigate("/", {replace: true})
-                    }} className="accountnav-menu menu-logout">Logout</MenuItem>
+                keepMounted
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                >
+                    <MenuItem 
+                        onClick={handleClose}
+                        component={Link}
+                        to={`/account/${user.id}`}
+                        className="accountnav-menu menu-account">
+                        Account</MenuItem>
+                    <MenuItem 
+                        onClick={handleClose}
+                        component={Link}
+                        to={`/projects`}
+                        className="accountnav-menu menu-projects">Projects</MenuItem>
+                    <MenuItem 
+                        onClick={() => {
+                            handleClose()
+                            localStorage.removeItem("current_user")
+                            navigate("/", {replace: true})
+                        }} 
+                        className="accountnav-menu menu-logout">Logout</MenuItem>
         </Menu>
     </>
-}
-
-// } 
+   
+} 
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -9,7 +9,23 @@ import "./NavBar.css"
 
 export const NavBar = () => {
 
+    const [user, setUser] = useState({})
+
+    const localUser = localStorage.getItem("current_user")
+    const currentUser = JSON.parse(localUser)
+
     const navigate = useNavigate()
+
+    useEffect(
+        () => {
+            getCurrentUser(currentUser.id)
+            .then((data) => {
+                const singleUser = data[0]
+                setUser(singleUser)
+            })
+        },
+        []
+    )
 
     return (
         <>
@@ -17,7 +33,7 @@ export const NavBar = () => {
                 <img src='https://res.cloudinary.com/dupram4w7/image/upload/v1663639606/Screen_Shot_2022-09-19_at_9.05.23_PM-removebg-preview_cvnxt3.png' alt="Home" className="home-image"></img>
             </button>
             <button onClick={() => navigate(`/projects`)} className="navbar__button navbar__projects">PROJECTS</button>
-            <AccountNav/>
+            <AccountNav user={user}/>
         </>
     )
 }
