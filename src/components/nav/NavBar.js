8ae96cc89a618214c1dee8e09d4b272988e12e84
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-20 19:01:35 -0500

    Navbar CSS

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -28,12 +28,14 @@ export const NavBar = () => {
     )
 
     return (
-        <>
+        <section className="navbar">
             <button onClick={() => navigate(`/`)} className="navbar__button navbar__home">
                 <img src='https://res.cloudinary.com/dupram4w7/image/upload/v1663639606/Screen_Shot_2022-09-19_at_9.05.23_PM-removebg-preview_cvnxt3.png' alt="Home" className="home-image"></img>
             </button>
-            <button onClick={() => navigate(`/projects`)} className="navbar__button navbar__projects">PROJECTS</button>
+            <section className="navbar-right">
+                <button onClick={() => navigate(`/projects`)} className="navbar__button navbar__projects">PROJECTS</button>
             <AccountNav user={user}/>
-        </>
+            </section>
+        </section>
     )
 }
