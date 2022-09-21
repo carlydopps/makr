2005c5999463938997221799210e93c935837ccd
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-20 22:49:49 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
+import { Footer } from "./components/footer/Footer"
 import { LandingPage } from "./components/home/LandingPage"
 import { Makr } from "./components/home/Makr"
 import { NavBar } from "./components/nav/NavBar"
@@ -10,14 +11,15 @@ import { Authorized } from "./components/views/Authorized"
 export const App = () => {
     return <>
         <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
             <Route path="*" element={
                 <>
                     <NavBar/>
                     <ApplicationViews/>
+                    <Footer/>
                 </>
             }/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
         </Routes>
     </>
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,7 @@
+import "./Footer.css"
+
+export const Footer = () => {
+    return <footer className="footer">
+        <img src='https://res.cloudinary.com/dupram4w7/image/upload/v1663639606/Screen_Shot_2022-09-19_at_9.05.23_PM-removebg-preview_cvnxt3.png' alt="Home" className="footer-image"></img>
+    </footer>
+}
