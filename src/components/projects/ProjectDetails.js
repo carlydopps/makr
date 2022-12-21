8a8aa164a1ed55846c82e29cadbbf71edd771d55
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-12-21 17:25:22 -0600

    Add status indicator

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -131,7 +131,9 @@ export const ProjectDetails = () => {
                             ?<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="svg-approved" viewBox="0 0 16 16">
                             <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
                             </svg>
-                            : <p>Denied</p>
+                            : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="svg-cancelled" viewBox="0 0 16 16">
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
                         : ""
                     }
                 </div>
